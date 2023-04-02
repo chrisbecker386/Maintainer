fae52ef7ac9b4e679f4e6c234d682bda8a9f3749
Christopher Becker <christopherbecker@online.de>
2023-04-02 07:23:52 +0200

    Initial commit

---
+++ app/src/main/java/de/chrisbecker386/maintainer/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package de.chrisbecker386.maintainer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//default android Color
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+//app app Color
+val WarmOrange = Color(0xFFCC5832)
+val Beige500 = Color(0xFFDDC7AD)
+val GrayGreen = Color(0xFF333936)
+val LightGray = Color(0xFF676867)
+val ExtraLightGray = Color(0xFFB3AFB2)
+val AiryWhite = Color(0xFFE9EDF0)
