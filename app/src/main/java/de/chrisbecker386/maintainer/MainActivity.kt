47a9ed93097711b2a944231a71b788210aa29772
Christopher Becker <christopherbecker@online.de>
2023-04-07 07:14:19 +0200

    add basic AppBar

--- app/src/main/java/de/chrisbecker386/maintainer/MainActivity.kt
+++ app/src/main/java/de/chrisbecker386/maintainer/MainActivity.kt
@@ -1,43 +1,53 @@
 package de.chrisbecker386.maintainer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import de.chrisbecker386.maintainer.ui.composable.MaintainerAppBar
 import de.chrisbecker386.maintainer.ui.theme.MaintainerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaintainerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MaintainerApp()
+        }
+    }
+
+    @Composable
+    fun MaintainerApp() {
+        MaintainerTheme {
+            Scaffold(topBar = {
+                MaintainerAppBar(
+                    title = "title",
+                    false,
+                    false,
+                    modifier = Modifier.height(40.dp)
+
+                )
+            }) { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaintainerTheme {
-        Greeting("Android")
+    @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+    @Composable
+    fun DefaultPreview() {
+        MaintainerApp()
     }
 }
--- app/src/main/java/de/chrisbecker386/maintainer/ui/theme/Color.kt
+++ app/src/main/java/de/chrisbecker386/maintainer/ui/theme/Color.kt
@@ -2,13 +2,13 @@ package de.chrisbecker386.maintainer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//default android Color
+// default android Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-//app app Color
+// app app Color
 val WarmOrange = Color(0xFFCC5832)
 val Beige500 = Color(0xFFDDC7AD)
 val GrayGreen = Color(0xFF333936)
