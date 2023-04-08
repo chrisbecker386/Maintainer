d5e500d21a58b4ca09417fcbd496f56303959b0e
Christopher Becker <christopherbecker@online.de>
2023-04-08 08:23:12 +0200

    enhanced app font

--- app/src/main/java/de/chrisbecker386/maintainer/ui/theme/Type.kt
+++ app/src/main/java/de/chrisbecker386/maintainer/ui/theme/Type.kt
@@ -1,11 +1,18 @@
 package de.chrisbecker386.maintainer.ui.theme
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.chrisbecker386.maintainer.R
 
@@ -39,82 +46,123 @@ val nunitoSansFont = FontFamily(
 val Typography = Typography(
     h1 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.ExtraBold,
-        fontSize = 96.sp,
+        fontWeight = FontWeight.Light,
+        fontSize = 106.sp,
         letterSpacing = (-1.5).sp
     ),
     h2 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.Bold,
-        fontSize = 60.sp,
+        fontWeight = FontWeight.Light,
+        fontSize = 66.sp,
         letterSpacing = (-0.5).sp
     ),
     h3 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.Bold,
-        fontStyle = FontStyle.Italic,
-        fontSize = 48.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 53.sp,
         letterSpacing = 0.sp
     ),
     h4 = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 34.sp,
+        fontSize = 37.sp,
         letterSpacing = 0.25.sp
     ),
     h5 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.Light,
-        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
         letterSpacing = 0.sp
     ),
     h6 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 20.sp,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
         letterSpacing = 0.15.sp
     ),
     subtitle1 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.Light,
-        fontStyle = FontStyle.Italic,
-        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
         letterSpacing = 0.15.sp
     ),
     subtitle2 = TextStyle(
         fontFamily = nunitoSansFont,
-        fontWeight = FontWeight.Light,
-        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
         letterSpacing = 0.1.sp
     ),
     body1 = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 18.sp,
         letterSpacing = 0.5.sp
     ),
     body2 = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
+        fontSize = 15.sp,
         letterSpacing = 0.25.sp
     ),
     button = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
+        fontSize = 15.sp,
         letterSpacing = 1.25.sp
     ),
     caption = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
+        fontSize = 13.sp,
         letterSpacing = 0.4.sp
     ),
     overline = TextStyle(
         fontFamily = nunitoSansFont,
         fontWeight = FontWeight.Normal,
-        fontSize = 10.sp,
+        fontSize = 11.sp,
         letterSpacing = 1.5.sp
     )
 )
+
+@Preview (showBackground = true)
+@Composable
+fun PreviewTyping() {
+    MaintainerTheme {
+        Column(Modifier.width(intrinsicSize =IntrinsicSize.Max).height(intrinsicSize = IntrinsicSize.Max)) {
+            val typeLevel = listOf<String>(
+                "h1",
+                "h2",
+                "h3",
+                "h4",
+                "h5",
+                "h6",
+                "subtitle1",
+                "subtitle2",
+                "body1",
+                "body2",
+                "button",
+                "caption",
+                "overline"
+            )
+            typeLevel.forEach { type ->
+                val style = when (type) {
+                    "h1" -> MaterialTheme.typography.h1
+                    "h2" -> MaterialTheme.typography.h2
+                    "h3" -> MaterialTheme.typography.h3
+                    "h4" -> MaterialTheme.typography.h4
+                    "h5" -> MaterialTheme.typography.h5
+                    "h6" -> MaterialTheme.typography.h6
+                    "subtitle1" -> MaterialTheme.typography.subtitle1
+                    "subtitle2" -> MaterialTheme.typography.subtitle2
+                    "body1" -> MaterialTheme.typography.body1
+                    "body2" -> MaterialTheme.typography.body2
+                    "button" -> MaterialTheme.typography.button
+                    "caption" -> MaterialTheme.typography.caption
+                    "overline" -> MaterialTheme.typography.overline
+                    else -> MaterialTheme.typography.h6
+                }
+                Text(text = "$type: Nunito Sans ", style = style,  maxLines = 1,modifier = Modifier.padding(8.dp))
+            }
+        }
+    }
+}
