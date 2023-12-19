a9983ad191660f12a84a2bfa3cd53fb31674d1f3
Christopher Becker <christopherbecker@online.de>
2023-12-19 20:44:48 +0100

    implement StepEditor (#78)

---
+++ app/src/main/java/de/chrisbecker386/maintainer/ui/component/IntervalPicker.kt
@@ -0,0 +1,75 @@
+/*
+ * Created by Christopher Becker on 27/12/2023, 10:04
+ * Copyright (c) 2023. All rights reserved.
+ * Last modified 27/12/2023, 10:04
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package de.chrisbecker386.maintainer.ui.component
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import de.chrisbecker386.maintainer.data.model.RepeatFrequency
+import de.chrisbecker386.maintainer.ui.component.basic.StepSliderEvent
+import de.chrisbecker386.maintainer.ui.component.basic.StepSliderState
+import de.chrisbecker386.maintainer.ui.component.basic.StepsSlider
+import de.chrisbecker386.maintainer.ui.theme.DIM_S
+import de.chrisbecker386.maintainer.ui.theme.MaintainerTheme
+
+@Composable
+fun IntervalPicker(
+    modifier: Modifier = Modifier,
+    state: StepSliderState,
+    onEvent: (StepSliderEvent) -> Unit = {}
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(DIM_S)
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = RepeatFrequency.values()[state.index].name,
+            style = typography.h2,
+            color = colors.onBackground
+        )
+        StepsSlider(
+            modifier = Modifier.fillMaxWidth(),
+            items = RepeatFrequency.values().toList(),
+            state = state,
+            onEvent = onEvent
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewIntervalPicker() {
+    MaintainerTheme {
+        IntervalPicker(
+            state = StepSliderState(3),
+            modifier = Modifier.background(color = colors.background)
+        )
+    }
+}
